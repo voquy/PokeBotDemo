8732632fb1bee5032704f4b170227d12e53346a6
voquy <quynh.evelyne@gmail.com>
2013-03-26 22:08:20 +0100

    PlusieursBots

--- src/main/java/fr/univaix/iut/pokebattle/run/PokemonMain.java
+++ src/main/java/fr/univaix/iut/pokebattle/run/PokemonMain.java
@@ -1,9 +1,14 @@
 package fr.univaix.iut.pokebattle.run;
 
+import fr.univaix.iut.pokebattle.bot.DresseurBot;
+import fr.univaix.iut.pokebattle.bot.JudgeBot;
 import fr.univaix.iut.pokebattle.bot.PokeBot;
 
 public class PokemonMain {
+
     public static void main(String[] args) {
-        BotRunner.runBot(new PokeBot(), "twitter4j.properties");
+        BotRunner.runBot(new PokeBot(), "PkmFantominus.properties");
+        BotRunner.runBot(new JudgeBot(), "JugeCordier.properties");
+        BotRunner.runBot(new DresseurBot(), "DrsPierre.properties");
     }
 }
