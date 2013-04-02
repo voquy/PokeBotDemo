7a630eb4c033500dcff8f2afc4a572f690f5dccd
Dounouw <dono13003@gmail.com>
2013-04-02 15:17:25 +0200

    F19 + "Sauvage" à la création

--- src/main/java/fr/univaix/iut/pokebattle/run/BotRunner.java
+++ src/main/java/fr/univaix/iut/pokebattle/run/BotRunner.java
@@ -3,11 +3,16 @@ package fr.univaix.iut.pokebattle.run;
 import java.io.IOException;
 import java.io.InputStream;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
 import com.twitter.hbc.httpclient.ControlStreamException;
 
 import fr.univaix.iut.pokebattle.bot.Bot;
+import fr.univaix.iut.pokebattle.bot.PokeBot;
 import fr.univaix.iut.pokebattle.tuse.Credentials;
 import fr.univaix.iut.pokebattle.twitter.TwitterBot;
+import fr.univaix.iut.pokebattle.twitter.TwitterBuilder;
 
 
 public class BotRunner {
@@ -24,7 +29,27 @@ public class BotRunner {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+		InputStream inputStream = getResourceAsStream("PkmFantominus.properties");
+        Credentials credentials = new Credentials();
+        
+		try {
+			credentials = Credentials.loadCredentials(inputStream);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		TwitterBuilder builder = new TwitterBuilder(credentials);
+        Twitter twitter = builder.build();
+        
+		try {
+			twitter.updateProfile(null, null, null,
+					"#pokebattle - #pokemon - #Etat: Sauvage - #Level: "+ PokeBot.level);
+			
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		  }
+	
+}
 
     static InputStream getResourceAsStream(String fileName) {
         return PokemonMain.class.getClassLoader().getResourceAsStream(fileName);
