ab148d69b15ec00b89f13f2748af16c4191d05dd
voquy <quynh.evelyne@gmail.com>
2013-04-02 14:53:27 +0200

    F20

---
+++ src/main/java/pokebattle/functions/JudgeCalculExpWin.java
@@ -0,0 +1,17 @@
+package pokebattle.functions;
+
+
+/**
+ * Reply to all.
+ */
+public class JudgeCalculExpWin {
+	
+	public static int fonction(String pkmGagnant, String pkmPerdant) {
+		// Code en dur, besoin de la persitence
+		/*
+		 int expWin = expPkmPerdant * lvlPkmGagnant / 7;
+		 */
+		return 65;
+	}
+
+}
