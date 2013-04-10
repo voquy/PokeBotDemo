7cb694c4a4f816d89eec66654405937193517b28
voquy <quynh.evelyne@gmail.com>
2013-04-10 17:25:20 +0200

    Test7

---
+++ src/test/java/fr/univaix/iut/pokebattle/smartcell/PokemonKoCellTest.java
@@ -0,0 +1,27 @@
+package fr.univaix.iut.pokebattle.smartcell;
+
+import fr.univaix.iut.pokebattle.bot.PokeBot;
+import fr.univaix.iut.pokebattle.twitter.Tweet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PokemonKoCellTest {
+
+    PokemonKoCell cell = new PokemonKoCell();
+
+    @Test
+    public void testReturnKo() {
+    	PokeBot.setPVRestant(0);
+        assertEquals("#KO /cc @viviane @dresseurAdv @quynhchee",
+        	cell.ask(new Tweet("viviane","@fantomiinus -10pv /cc @quynhchee", null)));
+    }
+    
+    @Test
+    public void testReturnNull() {
+    	PokeBot.setPVRestant(50);
+        assertEquals(null,
+        	cell.ask(new Tweet("quynhchee","", null)));
+    }
+}
